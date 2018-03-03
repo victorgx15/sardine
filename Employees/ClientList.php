6d1be202e2fa9f14182f39a65d833f82a44044b9
Sebastien Marchal <32772762+cbasz@users.noreply.github.com>
2018-03-03 13:59:29 +0100

    ClientList basic functions finished
    Can search/filter and sort each column alphabetically

--- Employees/ClientList.php
+++ Employees/ClientList.php
@@ -1,10 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style>
+        th {
+            cursor: pointer;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <br><input class="form-control" id="searchInput" type="text" placeholder="Search.."><br>
+    <script>
+        $(document).ready(function(){
+            $("#searchInput").on("keyup", function() {
+                var value = $(this).val().toLowerCase();
+                $("#clientTable tr").filter(function() {
+                    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+                });
+            });
+        });
+    </script>
 
-
+</div>
 
 <?php
-echo "<table style='border: solid 1px black;'>";
+echo "<table class='table table-striped table-bordered table-hover' id='clientTable'>";
 //A déterminer quels informations du client sont importants de display
-echo "<tr><th>Id</th><th>civilite</th><th>Nom</th><th>Prénom</th><th>Téléphone</th><th>E-mail</th></tr>";
+echo "<tr><th onclick='sortTable(0)' >Id</th><th onclick='sortTable(1)'>Civilite</th><th onclick='sortTable(2)'>Nom</th><th onclick='sortTable(3)'>Prénom</th><th onclick='sortTable(4)'>Téléphone</th><th onclick='sortTable(5)'>E-mail</th></tr>";
 
 class TableRows extends RecursiveIteratorIterator {
     function __construct($it) {
@@ -17,6 +44,7 @@ class TableRows extends RecursiveIteratorIterator {
 
     function beginChildren() {
         echo "<tr>";
+
     }
 
     function endChildren() {
@@ -25,6 +53,7 @@ class TableRows extends RecursiveIteratorIterator {
 }
 
 try {
+
     $bdd = new PDO('mysql:host=localhost;dbname=db;charset=utf8', 'root', '');
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $query="SELECT id, civilite, lastName, firstName, telephone, email FROM user ";
@@ -43,3 +72,63 @@ catch(PDOException $e) {
 $bdd = null;
 echo "</table>";
 ?>
+
+<script>
+    function sortTable(n) {
+        var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+        table = document.getElementById("clientTable");
+        switching = true;
+        //Set the sorting direction to ascending:
+        dir = "asc";
+        /*Make a loop that will continue until
+        no switching has been done:*/
+        while (switching) {
+            //start by saying: no switching is done:
+            switching = false;
+            rows = table.getElementsByTagName("TR");
+            /*Loop through all table rows (except the
+            first, which contains table headers):*/
+            for (i = 1; i < (rows.length - 1); i++) {
+                //start by saying there should be no switching:
+                shouldSwitch = false;
+                /*Get the two elements you want to compare,
+                one from current row and one from the next:*/
+                x = rows[i].getElementsByTagName("TD")[n];
+                y = rows[i + 1].getElementsByTagName("TD")[n];
+                /*check if the two rows should switch place,
+                based on the direction, asc or desc:*/
+                if (dir == "asc") {
+                    if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                        //if so, mark as a switch and break the loop:
+                        shouldSwitch= true;
+                        break;
+                    }
+                } else if (dir == "desc") {
+                    if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                        //if so, mark as a switch and break the loop:
+                        shouldSwitch= true;
+                        break;
+                    }
+                }
+            }
+            if (shouldSwitch) {
+                /*If a switch has been marked, make the switch
+                and mark that a switch has been done:*/
+                rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                switching = true;
+                //Each time a switch is done, increase this count by 1:
+                switchcount ++;
+            } else {
+                /*If no switching has been done AND the direction is "asc",
+                set the direction to "desc" and run the while loop again.*/
+                if (switchcount == 0 && dir == "asc") {
+                    dir = "desc";
+                    switching = true;
+                }
+            }
+        }
+    }
+</script>
+
+</body>
+</html>
