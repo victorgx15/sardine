e1152cf18d1d14ee6130590efc52ae2a4f7938ad
HarisDevJee <meskiharis@gmail.com>
2018-03-21 11:58:53 +0100

    paypal button pay (to fix it on card chekout

--- client/product_details.php
+++ client/product_details.php
@@ -127,7 +127,12 @@
 							</span></h5>
 						<p class="vote"><strong>91%</strong> of buyers enjoyed this product! <strong>(87 votes)</strong></p>
 					
-
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="KG2WQ7Z3AJRUU">
+<input type="image" src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal, le réflexe sécurité pour payer en ligne">
+<img alt="" border="0" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
+</form>
 
 						<?php 
 						//afficher bouton ajouter au panier quand il y a bien une quantité disponible 
